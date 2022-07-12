d447e561839f3f6c50de63bca76bcc09815087bb
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2022-07-12 23:34:21 +0100

    Edit code of design to Home Screen

--- lib/Screens/Home/Components/body.dart
+++ lib/Screens/Home/Components/body.dart
@@ -5,6 +5,8 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(),
+    );
   }
 }
