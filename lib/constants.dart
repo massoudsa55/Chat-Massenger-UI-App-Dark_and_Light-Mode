bb70a947996c4d812d991d8a329924223ae5cba7
Messaoud_Ben <33179371+massoudsa55@users.noreply.github.com>
2022-07-13 23:54:00 +0100

    Edit code

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const cPrimaryColor = Color(0xFF00BF6D);
+const cSecondaryColor = Color(0xFFFE9901);
+const cContentColorLightTheme = Color(0xFF1D1D35);
+const cContentColorDarkTheme = Color(0xFFF5FCF9);
+const cWarninngColor = Color(0xFFF3BB1C);
+const cErrorColor = Color(0xFFF03738);
+
+const cDefaultPadding = 20.0;
